db29dcc1a8099396499d6ffeb1f5ddb31d2b69d3
nagorecb <nagorecb@opendeusto.es>
2018-04-03 12:02:10 +0200

    operaciones upadted

--- src/LN/operaciones.c
+++ src/LN/operaciones.c
@@ -6,78 +6,59 @@
 
 t_Pregunta_Respuesta *listaTodasPreguntas;
 
-int calcularPuntos(t_jugador jugador, int * acumulada) 
+t_pregunta_respuestas generarPregunta (t_pregunta_respuestas *arrPreg, t_pregunta_respuestas* *preguntasSalidas, int sizePreguntasSalidas, int sizeTotalPreguntas)
 {
-	jugador.puntuacion = acumulada + jugador.puntuacion;
-}
-
-int sumarPunto(t_jugador jugador)
-{
-	jugador.puntuacion = jugador.puntuacion + 1;
-}
-
-t_pregunta_respuestas generarPregunta ( int cant_preguntas, t_pregunta_respuestas *preguntasSalidas)
-{
-	int random =0;
+	int random = 0;
 	t_pregunta_respuestas pregunta;
-	t_pregunta_respuestas *arrPreg=leerPregunta();
-	int repetida;
-	int cont;
+	int repetida = 1;
+	int cont = 0;
 	int MAX_INTENTOS = 20;
 	t_pregunta_respuestas *aux;
-	int sizePreguntasSalidas=0;
-	int sizeTotalPreguntas=0;
 
-
-	//Cantidad de preguntas en cada array
- 	sizePreguntasSalidas = sizeof(preguntasSalidas)/sizeof(t_pregunta_respuestas);
- 	sizeTotalPreguntas = sizeof(arrPreg)/sizeof(t_pregunta_respuestas);
-
-	srand(time(NULL));
- 	for (int i = 0; i < cant_preguntas; ++i)
-	 {
 	 	do
 	 	{
-		 		random = rand() %(cant_preguntas); //genera un número aleatorio entre 0 y cant_preguntas-1
+		 		random = rand() %(sizeTotalPreguntas); //genera un número aleatorio entre 0 y sizeTotalPreguntas-1
 		 		pregunta = arrPreg[random];
 
-		 		for(int j=0; j < sizePreguntasSalidas; i++)
+		 		for(int j = 0; j < sizePreguntasSalidas; i++)
 		 		{
-
-		 			//repetida = strcmp (pregunta.Pregunta,preguntasSalidas[i].Pregunta); //si son iguales, será 0
-		 			if((strcmp (pregunta.Pregunta,preguntasSalidas[i].Pregunta))==0)
+		 			//si son iguales, será 0
+		 			if((strcmp (pregunta.Pregunta, preguntasSalidas[i].Pregunta)) == 0)
 		 			{
-		 				repetida = 1; //si son iguales, será 0
+		 				repetida = 1; //si son iguales, será 1
 		 			}
 		 		}
+
 		 	cont++;
 	 	}
-	 	while(repetida==1 || cont ! = MAX_INTENTOS); //Hará esto hasta que encuentre una pregunta no repetida o agote la cantidad de intentos
+	 	while(repetida == 0 || cont ! = MAX_INTENTOS); //Hará esto hasta que encuentre una pregunta no repetida o agote la cantidad de intentos
 	 
-	 aux = (t_pregunta_respuestas*) malloc ((sizePreguntasSalidas+1)* sizeof(t_pregunta_respuestas));
+	 aux = (t_pregunta_respuestas*) malloc ((sizePreguntasSalidas+1) * sizeof(t_pregunta_respuestas));
 	 //creamos espacio para las preguntas ya salidas +1 (la nueva)
 	
 	//metemos todas las preguntas del array de los ya salidos en la variable auxiliar
 	 for (int i=0; i<sizePreguntasSalidas; i++)
 	 {
-	 	aux[i] = preguntasSalidas [i];
+	 	aux[i] = *preguntasSalidas [i];
 	 }
 	 aux[sizePreguntasSalidas] = pregunta;//Añadimos la pregunta en la variable auxiliar
 
-	 free (preguntasSalidas);
-	 preguntasSalidas = NULL;
+	 free (*preguntasSalidas);
+	 *preguntasSalidas = NULL;
 
 	 //Hacemos malloc de preguntasSalidas otra vez para darles espacio para un hueco más
-	 preguntasSalidas = (t_pregunta_respuestas*) malloc ((sizePreguntasSalidas+1)* sizeof(t_pregunta_respuestas));
+	 *preguntasSalidas = (t_pregunta_respuestas*) malloc ((sizePreguntasSalidas+1) * sizeof(t_pregunta_respuestas));
 	 
-	 for (int i=0; i<(sizePreguntasSalidas+1); i++)
+	 for (int i = 0; i<(sizePreguntasSalidas + 1); i++)
 	 {
-	 	preguntasSalidas [i] = aux[i]; 
+	 	*preguntasSalidas [i] = aux[i]; 
 	 }
 
 	 //nos libramos de aux porque ya no nos hace falta
 	 free(aux);
 	 aux = NULL;
+
+	 prepararRespuestas(&pregunta);
 
 	 return pregunta;
 }
@@ -102,18 +83,17 @@ int comprobarRespuesta(char* respuesta)
 	return correcto;
 }
 
-int maxPreguntas (int cantPreguntas) 
 //leer todas las preguntas ver cuántas hay del fichero
 //lo que hay del fichero pasar a array
 //comparar lo del parám con la cantidad de preguntas
 //para que no me pida más preguntas de las que hay
-{
-	listaTodasPreguntas = leerPreguntas();
-	
+//CUANDO ES MULTIJGADOR LO MANDAMOS a cantpreguntas la cantpreguntas * el numdejugadores
+int maxPreguntas (int cantPreguntas, int sizeTotalPreguntas) 
+{	
 	int valido;	//si es 1 es válido, si es 0 no lo es.
 	int size;
 
-	size = sizeof(listaTodasPreguntas)/sizeof(t_Pregunta_Respuesta);
+	size = sizeTotalPreguntas;
 
 	if(size < cantPreguntas)
 	{
@@ -127,30 +107,37 @@ int maxPreguntas (int cantPreguntas)
 	return valido;
 }
 
+ void ordenarJugadores(t_jugador* jugadoresLeidos, int size)
+ {
+ 	t_jugador temp; //variable auxiliar
 
-Pregunta_Respuesta generarPregunta(Pregunta_Respuesta *listaPreguntas) 
+ 	// recorrer todos los elementos del array
+   for(int i = 0; i < size-1; i++) 
+   { 
+      //comparar cada elemento con todos los demás
+      for(int j = 0; j < size-1-i; j++)
+      {
+        if(jugadoresLeidos[j].puntuacion > jugadoresLeidos[j+1].puntuacion)
+         {
+            temp = jugadoresLeidos[j];
+            jugadoresLeidos[j] = jugadoresLeidos[j+1];
+            jugadoresLeidos[j+1] = temp;	
+         }		
+      }
+ 	}
+}
+
+int maxPuntuacion(t_jugador* jugadores, int sizeJugadores)
 {
-	//La operación módulo (%) nos da el resto de dividir rand() entre 11. Este resto puede ir de 0 a 10. 
-	//De la misma forma, el módulo de rand() entre N+1 va de 0 a N.
-	//NO SÉ CUÁNTAS PREGUNTAS TENDREMOS AL FINAL +_+, pero eso sería N
+	int max =jugadores[0].puntuacion;
 
-	/*leer el fichero de preguntas y coger 
-	una pregunta aleatoria que no esté en la lista 
-	que nos pasan y añadir esa nueva pregunta 
-	en la lista para que en la siguiente 
-	no se genere tampoco esa. 
-	Añadirla a la lista después de llamar 
-	al método prepararRespuestas(), es decir, 
-	con las respuestas ya desordenadas
-	*/
-
-	leerPreguntas(listaPreguntas);
-
-	//coger  una pregunta aleatoria que no esté en la lista que nos pasan
-	
-	//asignarles ún número a cada pregunta en listaTodasPreguntas para que se pueda escoger una aleatorio con el módulo de rand()
-
-	if ()//esa pregunta coincide con preguntas de listaTodasPreguntas)
+	for(int i=0; i < sizeJugadores; i++)
 	{
-
+		if(max > jugadores[i].puntuacion)
+		{
+			max = jugadores[i].puntuacion;
+		}
 	}
+	return max;
+}
+
