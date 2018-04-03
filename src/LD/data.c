891bb02b0361c9532a92aeee4fc0648aa717f357
nagorecb <nagorecb@opendeusto.es>
2018-04-03 11:17:52 +0200

    data.c and operaciones.c

--- src/LD/data.c
+++ src/LD/data.c
@@ -1,1 +1,96 @@
 #include "data.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
++guardarJugador(jugador : *Jugador) : void
++leerJugador() : *Jugador
++leerPreguntas(preguntas : Pregunta_Respuesta) : void
+*/
+
+FILE * file;
+
+//---- fichero llamado "input.txt" con jugadores y su puntuación
+
+if ()//file es un txt
+{
+  leerJugador();
+  leerPreguntas();
+}
+else //file es un binario
+{
+
+}
+
+t_preguntasRespuestas* leerPreguntas(Pregunta_Respuesta preguntas) //este en binario
+{
+  file = fopen ("input.txt", "rb");
+  
+  if (file == NULL) 
+  {
+    printf("Archivo no encontrado\n");
+    return -1;
+  }
+}
+
+void guardarJugador(t_jugador* jugadores)
+{
+  FILE* f;
+  int c;
+  
+  //abrir fichero para escritura "w"
+  f = fopen("jugador.txt", "w");
+  
+  //escribir en fichero un string formateado 
+  //un for en el que me recorro todo el array de jugadores de param
+  //por cada hueco del array --> string: nick,puntuacion
+  //crear un string auxiliar1 a ese string dare el valor del nick en cada momento 
+  //crear un string auxiliar2 a la posición 0 del string auxiliar "," y a la posición 1 darle 0 o /0 
+  //luego hacer strcat (para concatenar) paso 1ro aux1 y 2o el aux2
+  //cargarme el aux2 y que aux2 valga puntuación en cada momento
+  //luego hacer strcat (para concatenar) paso 1ro aux1 y 2o el aux2
+  //cargarme el aux2 y que valga /n en la posición 0 y en la posi 1 que valga /0
+  //luego hacer strcat (para concatenar) paso 1ro aux1 y 2o el aux2
+  //por cada hueco --> fprintf(f, aux1, MAX_NUM (me lo  invento el max num));
+  
+  //cerrar fichero
+  fclose(f);
+}
+
+leerJugador()
+{
+  FILE* f;
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  int cont =0;
+  int puntuacion;
+  string aux;
+
+  f = fopen("jugador.txt", "r");
+
+  while ((read = getline(&line, &len, f)) != -1) 
+  {
+
+  }
+
+  while ((read = getline(&line, &len, f)) != -1) 
+  {
+    //con el cont de arriba sé cuantas lineas tengo en el fich
+
+    //me recorro el line en un for caracter a caracter y voy guardando estos en un string hasta que llegue a la coma,
+    //cuando llegue, el string se lo asigno a la posición jugadores[cont].nick del array de jugadores que voy a devolver
+    //pasarlo todo a un string con scanf(puntuacion,%i, aux) //aux es el strign anterior
+    //cojo todos los caracteres de después de la coma se los asigno a jugadores [cont].puntuacion
+
+    cont ++;
+
+  }
+
+  //cerrar fichero
+  fclose(f);
+
+  retur t_jugador
+}
--- src/LN/operaciones.c
+++ src/LN/operaciones.c
@@ -1,1 +1,156 @@
-#include "operaciones.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "INCLUDES/operaciones.h"
+
+t_Pregunta_Respuesta *listaTodasPreguntas;
+
+int calcularPuntos(t_jugador jugador, int * acumulada) 
+{
+	jugador.puntuacion = acumulada + jugador.puntuacion;
+}
+
+int sumarPunto(t_jugador jugador)
+{
+	jugador.puntuacion = jugador.puntuacion + 1;
+}
+
+t_pregunta_respuestas generarPregunta ( int cant_preguntas, t_pregunta_respuestas *preguntasSalidas)
+{
+	int random =0;
+	t_pregunta_respuestas pregunta;
+	t_pregunta_respuestas *arrPreg=leerPregunta();
+	int repetida;
+	int cont;
+	int MAX_INTENTOS = 20;
+	t_pregunta_respuestas *aux;
+	int sizePreguntasSalidas=0;
+	int sizeTotalPreguntas=0;
+
+
+	//Cantidad de preguntas en cada array
+ 	sizePreguntasSalidas = sizeof(preguntasSalidas)/sizeof(t_pregunta_respuestas);
+ 	sizeTotalPreguntas = sizeof(arrPreg)/sizeof(t_pregunta_respuestas);
+
+	srand(time(NULL));
+ 	for (int i = 0; i < cant_preguntas; ++i)
+	 {
+	 	do
+	 	{
+		 		random = rand() %(cant_preguntas); //genera un número aleatorio entre 0 y cant_preguntas-1
+		 		pregunta = arrPreg[random];
+
+		 		for(int j=0; j < sizePreguntasSalidas; i++)
+		 		{
+
+		 			//repetida = strcmp (pregunta.Pregunta,preguntasSalidas[i].Pregunta); //si son iguales, será 0
+		 			if((strcmp (pregunta.Pregunta,preguntasSalidas[i].Pregunta))==0)
+		 			{
+		 				repetida = 1; //si son iguales, será 0
+		 			}
+		 		}
+		 	cont++;
+	 	}
+	 	while(repetida==1 || cont ! = MAX_INTENTOS); //Hará esto hasta que encuentre una pregunta no repetida o agote la cantidad de intentos
+	 
+	 aux = (t_pregunta_respuestas*) malloc ((sizePreguntasSalidas+1)* sizeof(t_pregunta_respuestas));
+	 //creamos espacio para las preguntas ya salidas +1 (la nueva)
+	
+	//metemos todas las preguntas del array de los ya salidos en la variable auxiliar
+	 for (int i=0; i<sizePreguntasSalidas; i++)
+	 {
+	 	aux[i] = preguntasSalidas [i];
+	 }
+	 aux[sizePreguntasSalidas] = pregunta;//Añadimos la pregunta en la variable auxiliar
+
+	 free (preguntasSalidas);
+	 preguntasSalidas = NULL;
+
+	 //Hacemos malloc de preguntasSalidas otra vez para darles espacio para un hueco más
+	 preguntasSalidas = (t_pregunta_respuestas*) malloc ((sizePreguntasSalidas+1)* sizeof(t_pregunta_respuestas));
+	 
+	 for (int i=0; i<(sizePreguntasSalidas+1); i++)
+	 {
+	 	preguntasSalidas [i] = aux[i]; 
+	 }
+
+	 //nos libramos de aux porque ya no nos hace falta
+	 free(aux);
+	 aux = NULL;
+
+	 return pregunta;
+}
+
+//Verificar si la respuesta elegida es la correcta
+//(ver si la respuesta a la que se le ha asignado el valor de la letra que se ha elegido empieza por punto)
+int comprobarRespuesta(char* respuesta) 
+{
+	int correcto;
+
+	//si correcto es 1, ha contestado bien
+
+	if (respuesta[0] =='.')
+	{
+		correcto =1;
+	}
+	else
+	{
+		correcto =0;
+	}
+
+	return correcto;
+}
+
+int maxPreguntas (int cantPreguntas) 
+//leer todas las preguntas ver cuántas hay del fichero
+//lo que hay del fichero pasar a array
+//comparar lo del parám con la cantidad de preguntas
+//para que no me pida más preguntas de las que hay
+{
+	listaTodasPreguntas = leerPreguntas();
+	
+	int valido;	//si es 1 es válido, si es 0 no lo es.
+	int size;
+
+	size = sizeof(listaTodasPreguntas)/sizeof(t_Pregunta_Respuesta);
+
+	if(size < cantPreguntas)
+	{
+		valido =0;
+	}
+	else
+	{
+		valido =1;
+	}
+
+	return valido;
+}
+
+
+Pregunta_Respuesta generarPregunta(Pregunta_Respuesta *listaPreguntas) 
+{
+	//La operación módulo (%) nos da el resto de dividir rand() entre 11. Este resto puede ir de 0 a 10. 
+	//De la misma forma, el módulo de rand() entre N+1 va de 0 a N.
+	//NO SÉ CUÁNTAS PREGUNTAS TENDREMOS AL FINAL +_+, pero eso sería N
+
+	/*leer el fichero de preguntas y coger 
+	una pregunta aleatoria que no esté en la lista 
+	que nos pasan y añadir esa nueva pregunta 
+	en la lista para que en la siguiente 
+	no se genere tampoco esa. 
+	Añadirla a la lista después de llamar 
+	al método prepararRespuestas(), es decir, 
+	con las respuestas ya desordenadas
+	*/
+
+	leerPreguntas(listaPreguntas);
+
+	//coger  una pregunta aleatoria que no esté en la lista que nos pasan
+	
+	//asignarles ún número a cada pregunta en listaTodasPreguntas para que se pueda escoger una aleatorio con el módulo de rand()
+
+	if ()//esa pregunta coincide con preguntas de listaTodasPreguntas)
+	{
+
+	}
